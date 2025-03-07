641a44b69281d840837d5c9a4864be5f8c514a7f
Sumit Khinchi <khinchi.skumar@gmail.com>
2025-03-08 01:34:50 +0530

    Update TwoSum.js

--- Day1/TwoSum.js
+++ Day1/TwoSum.js
@@ -1,21 +1,24 @@
 console.log("Welcome to Blind 75 Day 1");
 
-function TwoSum(arr,target) {
-    let myMap =  new Map();
-    console.log(myMap);
-    for(let i=0;i<arr.length;i++)
-    {
-        let x= target-arr[i];
-        if(myMap.has(x)){
-            return [myMap.get(x),i]
-        }
-        else {
-            myMap.set(arr[i],i)
-        }
-
+function TwoSum(arr, target) {
+  let myMap = new Map();
+  console.log(myMap);
+  for (let i = 0; i < arr.length; i++) {
+    let x = target - arr[i];
+    // If the current number is already in the map, it means we have found a pair
+        // Check if the complement (target - current number) exists in the map
+    if (myMap.has(x)) {     
+      console.log("Accepted/ Correct Answer", [myMap.get(x), i]);
+      return [myMap.get(x), i];
+    } 
+    
+    else {
+      console.log("Rejected Values", arr[i], i);
+      myMap.set(arr[i], i);
     }
+  }
 }
 
-let arr= [2,7,4,5,11,15];
-let target =17;
-console.log(TwoSum(arr,target));
+let arr = [2, 7, 4, 5, 11, 15];
+let target = 17;
+console.log(TwoSum(arr, target));
